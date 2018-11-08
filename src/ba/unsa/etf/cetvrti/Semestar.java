f8ef92ccc710ad4ba52f02d19e3512c97bde0a7f
student <hhaskovic1@etf.unsa.ba>
2018-11-08 10:10:23 +0100

    ispravke

--- src/ba/unsa/etf/cetvrti/Semestar.java
+++ src/ba/unsa/etf/cetvrti/Semestar.java
@@ -3,21 +3,19 @@ package ba.unsa.etf.cetvrti;
 public class Semestar {
     private String nazivSemestra = "";
     private Predmet[] predmeti = null;
+    static int br=10;
 
     public Semestar(String naziv, Predmet[] obavezni) {
 
         this.nazivSemestra=naziv;
         predmeti=new Predmet[12];
-        for(int i=0; i<12; i++) {
+        for(int i=0; i<10; i++) {
             predmeti[i].setNazivPredmeta(obavezni[i].getNazivPredmeta());
             predmeti[i].setBrojStudenata(obavezni[i].getBrojStudenata());
             predmeti[i].setECTSBodovi(obavezni[i].getECTSBodovi());
         }
 
     }
-
-
-
 
     public String getNazivSemestra() {
         return nazivSemestra;
@@ -27,7 +25,11 @@ public class Semestar {
         this.nazivSemestra = nazivSemestra;
     }
     public void dodajIzborni(Predmet izborni){
-        return;
+
+        predmeti[br].setNazivPredmeta(izborni.getNazivPredmeta());
+        predmeti[br].setBrojStudenata(izborni.getBrojStudenata());
+        predmeti[br].setECTSBodovi(izborni.getECTSBodovi());
+        br++;
     }
 
     public String ispisi(){
